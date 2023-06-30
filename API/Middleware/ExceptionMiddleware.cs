61036b2fe3cbc83ee07c0f4e1894d145bf0cf20f
Diego Oldenburg <doldenburg@tulane.edu>
2023-06-29 19:59:47 -0500

    Edit photos functionality

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -9,12 +9,14 @@ namespace API.Middleware
         private readonly RequestDelegate _next;
         private readonly ILogger<ExceptionMiddleware> _logger;
         private readonly IHostEnvironment _env;
-        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger,
+            IHostEnvironment env)
         {
             _env = env;
             _logger = logger;
             _next = next;
         }
+
         public async Task InvokeAsync(HttpContext context)
         {
             try
@@ -32,7 +34,9 @@ namespace API.Middleware
                     : new ApiException(context.Response.StatusCode, ex.Message, "Internal Server Error");
 
                 var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
                 var json = JsonSerializer.Serialize(response, options);
+
                 await context.Response.WriteAsync(json);
             }
         }
