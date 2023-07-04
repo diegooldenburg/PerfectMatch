604287480ad89971510be05692b7fbbe71bd422c
Diego Oldenburg <doldenburg@tulane.edu>
2023-07-04 16:14:35 -0500

    paging and more caching

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { User } from './models/user';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = 'FragOut';
+  title = 'PerfectMatch';
   constructor(private accountService: AccountService) {}
   ngOnInit(): void {
     this.setCurrentUser();
